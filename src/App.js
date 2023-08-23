8dd8f6fde3e0e4445ea9f3ce51b4a382c6d17961
usm2198 <101632664+usm2198@users.noreply.github.com>
2023-08-23 18:10:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card1Container from './container/Card1Container';
+import CardContainer from './container/CardContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <CardContainer />
+      <Card1Container />
+    </div>
+  );
+}
+
+export default App;
